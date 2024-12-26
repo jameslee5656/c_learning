0311b54228c818ffd4382367eeb02b0b9a8dce89
James Lee <james.lee56566@gmail.com>
2024-12-26 22:35:28 +0800

    stringMatch: update

--- stringMatch/kmpSearch.cpp
+++ stringMatch/kmpSearch.cpp
@@ -2,9 +2,9 @@
 #include "string.h"
 #include "kmpSearch.hpp"
 
-int lps[PATTERN_MAX_LEN];
+int next[PATTERN_MAX_LEN];
 
-int createLpsTable(char *psPattern)
+int createNextTable(char *psPattern)
 {
     int i, j;
     int nPatternLen = 0;
@@ -14,38 +14,61 @@ int createLpsTable(char *psPattern)
     if (nPatternLen > PATTERN_MAX_LEN)
         return ERROR_GENERAL;
 
-    memset(lps, 0, PATTERN_MAX_LEN);
+    memset(next, 0, PATTERN_MAX_LEN);
 
     // 1. for loop each index of the pattern, find it's lps
     for (i = 1; i < nPatternLen; ++i)
     {
         // 2. Initialize j as the previous lps value
-        j = lps[i - 1];
+        j = next[i - 1];
 
-        // 3. jumping to longest previous longest previous location
+        // 3. jumping to longest previous location
         while (j > 0 && psPattern[i] != psPattern[j])
-            j = lps[j - 1];
+            j = next[j - 1];
 
         // 4. if the index match, our prefix and suffix increased
         if (psPattern[i] == psPattern[j])
             ++j;
 
-        lps[i] = j;
+        next[i] = j;
     }
+
+    // 5. move to the right to create next table, and set the first one as -1
+    memmove(next, &next[1], nPatternLen);
+    next[0] = -1;
 }
 
 // 傳入參數: char *psPattern, char *psBible
 int kmpSearch(char *psPattern, char *psBible)
 {
+    // 1. 建立參數:
+    int nPatternIdx = 0, nBibleIdx = 0;
+    int nPatternLen, nBibleLen;
+
+    nPatternLen  = strnlen(psPattern, PATTERN_MAX_LEN);
+    nBibleLen = strnlen(psBible, PATTERN_MAX_LEN);
+
+    // 2. 為 pattern 建立 next 表
+    createNextTable(psPattern);
+
+    // 3. while loop until Bible end
+    while (psBible[nBibleIdx] != '\0')
+    // 4.1. if (nBibleIndex < 0) or (pcPattern[nPatternIndex] == pcBible[nBibleIndex])
+    //       ++nBibleIndex;
+    //       ++ nPatternIndex;
+    //         4.1.1. pcBible[nBibleIndex] == end
+    // 	      ++result
+    //             nPatternIndex = next[nPatternIndex]
+    // 4.2.  else
+    //       nPatternIndex = next[nPatternIndex]
+    //  5. return result
 
 }
 
 
-// 1.	建立參數:
 // nPatternIndex = 0, nPatternIndex 代表 pattern 現在的位置
 // nBibleIndex = 0, nBibleIndex 代表 bible 現在的位置
 // nResult = 0, nResult代表 bible 當中 match 的次數
-// 2.	為 pattern 建立 next 表
 // 3.	for loop pcBible[nBibleIndex ] != ‘\0’
 // 4.1. if (nBibleIndex < 0) or (pcPattern[nPatternIndex] == pcBible[nBibleIndex])
 //       ++nBibleIndex;
